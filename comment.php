d098e0a99e12c9f15053beee6789f20a7896dc61
najo17 <fauzananris17@gmail.com>
2025-12-10 07:41:00 +0700

    Update comment.php

--- comment.php
+++ comment.php
@@ -1,15 +1,25 @@
 <?php
+
+// Memulai session
 session_start();
+
+// Memanggil file koneksi database
 include "config.php";
 
-$foto = $_POST['fotoid'];
-$isi  = $_POST['isi'];
-$uid  = $_SESSION['UserID'];
+// Mengambil data dari form
+$foto = $_POST['fotoid'];    // ID foto yang dikomentari
+$isi  = $_POST['isi'];       // Isi komentar
+$uid  = $_SESSION['UserID']; // ID user yang sedang login
+
+// Mengambil tanggal saat ini
 $tgl  = date("Y-m-d");
 
+// Menyimpan komentar ke database
 mysqli_query($koneksi,
     "INSERT INTO komentarfoto VALUES('', '$foto', '$uid', '$isi', '$tgl')"
 );
 
+// Redirect kembali ke halaman detail foto
 header("Location: detail.php?id=$foto");
+
 ?>
