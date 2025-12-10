346c6fc050949fe32a72ce3bc10975a9abbb445b
najo17 <fauzananris17@gmail.com>
2025-12-10 07:28:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
+// Memulai session agar bisa mengecek status login user
 session_start();
+
+// Jika session UserID ada → berarti sudah login
 if(isset($_SESSION['UserID'])){
+    
+    // Arahkan user ke halaman utama (home)
     header("Location: home.php");
+
 } else {
+    // Jika belum login → arahkan ke halaman login
     header("Location: login.php");
 }
 ?>
+
