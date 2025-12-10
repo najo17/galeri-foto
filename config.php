619283b1a30906a6c14242af21477c2d55cba6f4
najo17 <fauzananris17@gmail.com>
2025-12-10 07:39:57 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,13 @@
 <?php
+
+// Membuat koneksi ke database MySQL
+// Parameter: (host, username, password, nama_database)
 $koneksi = mysqli_connect("localhost", "root", "", "gallery");
 
+// Mengecek apakah koneksi berhasil atau gagal
 if(!$koneksi){
+    // Jika gagal, hentikan program dan tampilkan pesan error
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
+
 ?>
